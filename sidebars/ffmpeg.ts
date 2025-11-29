d23ae7596075d0c0ad82d2959f80fd94581f8e21
magicianlib <magicianlib@gmail.com>
2025-11-29 13:38:14 +0800

    update

--- sidebars/ffmpeg.ts
+++ sidebars/ffmpeg.ts
@@ -4,25 +4,36 @@ const sidebars: SidebarsConfig = {
   ffmpeg: [
     {
       type: 'category',
-      label: '媒体文件介绍',
+      label: '快速开始',
+      items: [
+        '安装 ffmpeg',
+        'ffmpeg 截取视频',
+        'ffmpeg 合并多个视频文件',
+      ],
+    },
+    {
+      type: 'category',
+      label: '流媒体介绍',
       items: [
         '视频文件工作的基本原理',
         '视频编解码器介绍',
         '音频编解码器介绍',
         '常见的视频容器格式',
-
       ],
     },
-    '安装 ffmpeg',
-    'ffprobe 查看媒体文件头信息',
-    '如何确认视频文件流媒体信息',
     {
       type: 'category',
-      label: '流媒体操作',
+      label: 'ffprobe 命令',
       items: [
-        'ffmpeg 截取视频',
+        'ffprobe 查看媒体文件头信息',
+        '如何确认视频文件流媒体信息',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'ffmpeg 进阶使用',
+      items: [
         'ffmpeg 的 map 参数',
-        'ffmpeg 合并多个视频文件',
         'ffmpeg 修改流媒体 Metadata',
         'ffmpeg 视频添加音频',
         'ffmpeg 视频添加字幕',
