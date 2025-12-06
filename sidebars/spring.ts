176b6b5c34ebb3e93f95124968a639750c3d342f
magicianlib <magicianlib@gmail.com>
2025-12-06 13:52:20 +0800

    Spring 目录调整

--- sidebars/spring.ts
+++ sidebars/spring.ts
@@ -3,8 +3,10 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
     spring: [
         '@Import 的三种使用方式',
+        'ImportSelector 与自动装配的实现',
         'Spring 事件与异步监听器',
         '容器初始化流程源码解析',
+        '自动装配的四种模式',
         '依赖注入与注入模式',
         'Spring 如何判断事务是否生效',
         'Spring 强制使用 cglib 的好处',
@@ -16,15 +18,7 @@ const sidebars: SidebarsConfig = {
         'Spring Bean 循环依赖源码解析',
         'Spring 的几种扩展方式',
         '使用 BeanNameGenerator 改变 BeanName 生成策略',
-        'ImportSelector 与自动装配的实现',
-        {
-            type: 'category',
-            label: 'SpringBoot',
-            items: [
-                'SpringBoot/自定义 Configuration Properties',
-                'SpringBoot/自动装配的四种模式',
-            ],
-        },
+        '自定义 Configuration Properties',
     ],
 };
 
