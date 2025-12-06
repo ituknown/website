66e6f2aaa364e924e77ae1efbc54125b6edda546
magicianlib <magicianlib@gmail.com>
2025-12-06 12:38:39 +0800

    依赖注入与注入模式

--- sidebars/spring.ts
+++ sidebars/spring.ts
@@ -5,7 +5,6 @@ const sidebars: SidebarsConfig = {
         '@Import 的三种使用方式',
         'Spring 事件与异步监听器',
         '容器初始化流程源码解析',
-        '依赖注入',
         '依赖注入与注入模式',
         'Spring 如何判断事务是否生效',
         'Spring 强制使用 cglib 的好处',
