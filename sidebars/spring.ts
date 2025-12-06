afd71886b5960815ea36afde46a784ecfc2e5e28
magicianlib <magicianlib@gmail.com>
2025-12-06 11:55:30 +0800

    容器初始化流程源码解析

--- sidebars/spring.ts
+++ sidebars/spring.ts
@@ -4,6 +4,7 @@ const sidebars: SidebarsConfig = {
     spring: [
         '@Import 的三种使用方式',
         'Spring 事件与异步监听器',
+        '容器初始化流程源码解析',
         {
             type: 'category',
             label: 'SpringBoot',
